3e1a689cb1e25fcc94280365458993f796b8502f
odipojames <odipojames12@gmai.com>
2018-08-13 10:15:22 +0300

    change date logic

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,7 +8,7 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
       let today:Date = new Date(); //get current date and time
       let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-      var dateDifference =Math.abs(todayWithNoTime-value )// returns value in milliseconds
+      var dateDifference =Math.abs(value-todayWithNoTime)// returns value in milliseconds
       const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
 
       var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
@@ -18,7 +18,7 @@ export class DateCountPipe implements PipeTransform {
       if (dateCounter >=1){
           return dateCounter;
       }else{
-          return 6;
+          return 0;
       }
     }
   }
