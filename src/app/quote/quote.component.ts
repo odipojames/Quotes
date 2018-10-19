a72ff3a68dbbcf6c5a545d4567e2f7a99b4c3b40
odipojames <odipojames12@gmai.com>
2018-10-19 15:49:26 +0300

    add confirmation button for delete quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,15 +9,19 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-        // new Quote(1,'Money  cant buy life ','by Bob Marley',new Date(2018,3,4)),
-        // new Quote(1,'Love has no bundry','by Prof. Akoto',new Date(2018,3,1)),
-        //   new Quote(1,'Nobody chose biological parents','by Akothee',new Date(2017,3,14)),
+         new Quote(1,'Money  cant buy life ','by Bob Marley',new Date(2018,3,4),0,0),
+         new Quote(1,'Love has no bundry','by Prof. Akoto',new Date(2018,3,1),0,0),
+          new Quote(1,'Nobody chose biological parents','by Akothee',new Date(2017,3,14),0,0),
         ]
 seenQuote(isSeen,index){
         if (isSeen){
+          let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+          if(toDelete){
+
             this.quotes.splice(index,1);
             }
           }
+        }
           toogleDetails(index){
         this.quotes[index].showDescription = !this.quotes[index].showDescription;
     }
